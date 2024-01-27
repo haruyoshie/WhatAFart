52f98712b7e4cdb3601e9efbf339dd63fae30b2d
Haruyoshie <haruyoshie.ecimac@utadeo.edu.co>
2024-01-27 17:22:15 -0500

    sounds
    farts sounds

--- What a Fart/Assets/Scripts/ChangeScene.cs
+++ What a Fart/Assets/Scripts/ChangeScene.cs
@@ -2,12 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 public class ChangeScene : MonoBehaviour
 {
-  
+   public AudioClip[] audioClips;
+   public AudioSource audioSource;
   public void changeScene(string sceneName)
   {
     SceneManager.LoadScene(sceneName);
+  }
+
+  public void selectRandom()
+  {
+      int fart = Random.Range(0, audioClips.Length);
+      audioSource.clip = audioClips[fart];
+      audioSource.Play();
   }
 }
